998dca32964f5d4dee0e3a94ef2344cec728fb36
Daniel Washington <danielvertigo@hotmail.com>
2021-06-11 13:14:51 -0300

    Files

--- AretheNumbersEqual/ViewController.swift
+++ AretheNumbersEqual/ViewController.swift
@@ -5,6 +5,22 @@
 //  Created by Daniel Washington Ignacio on 11/06/21.
 //
 
+
+/*
+ Create a function that returns true when num1 is equal to num2; otherwise return false.
+
+ Examples
+
+ isSameNum(4, 8) ➞ false
+
+ isSameNum(2, 2) ➞  true
+
+ isSameNum(2, 3) ➞ false
+ Notes
+
+ Don't forget to return the result.
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +28,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(self.isSameNum(4, 8))
+        print(self.isSameNum(2, 2))
+        print(self.isSameNum(2, 3))
     }
-
+   
+    func isSameNum(_ num1: Int, _ num2: Int) -> Bool {
+        return num1 == num2
+    }
+    
 
 }
 
